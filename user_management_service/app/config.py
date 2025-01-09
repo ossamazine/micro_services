01eff1cf794f1797ab4382380f4befefe2236e77
Ossama Zine-Eddine <ossamazineeddine@gmail.com>
2025-01-09 21:29:30 +0100

    Ajout de la fonctionnalité de gestion des utilisateurs

--- user_management_service/app/config.py
+++ user_management_service/app/config.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+mysqlconnector://root:@localhost/user_db")
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost/user_db"
+# Database configuration
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+mysqlconnector://root:@host.docker.internal:3306/user_db")
 
+# JWT configuration
 SECRET_KEY = os.getenv("SECRET_KEY", "bbd01a4b442b02a71985b200f7b5257cbdf145d5b8ff61916c0768cfc39f8281")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
